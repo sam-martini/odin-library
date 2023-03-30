577d8be66f524dfb7f9b6e487d3ddd382f1abc38
sam-martini <113351225+sam-martini@users.noreply.github.com>
2023-03-30 18:22:57 +1100

    Define book contructor; Add book to library

--- script.js
+++ script.js
@@ -5,8 +5,32 @@ const closeModal = document.querySelector('.close-btn');
 
 openModal.addEventListener('click', () => {
     modal.showModal();
-})
+});
 
 closeModal.addEventListener('click', () => {
     modal.close();
-})
+});
+
+
+// Create an empty array 'myLibrary' that will store the book objects.
+let myLibrary = [];
+
+// Define a 'Book' constructor function that takes in arguments for a title, author, number of pages and if it has been read or not.
+// Inside the constructor, assign each argument to a corresponding property on the newly created 'Book' object.
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+// Write a 'addBookToLibrary' function that takes arguments for the title, author, number of pages and read-status.
+// Use the 'Book' constructor to create the new 'Book' object with the input.
+// Push the new 'Book' object to the 'myLibrary' array.
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary('DragonBall', 'Akira Toriyama', 192, true);
+console.log(myLibrary);
